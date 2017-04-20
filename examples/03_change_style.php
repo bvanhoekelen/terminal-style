6b71ff04f5e1f71a9ffb071b34eb42fc1cabc8ce
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-04-20 19:56:53 +0200

    Add examples

---
+++ examples/03_change_style.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Require vendor autoload
+ */
+require_once('../vendor/autoload.php');
+
+/*
+ * Change text to bold
+ */
+echo " > " . terminal_style('This text will be bold', null, null, 'bold') . PHP_EOL;
+
+/*
+ * Change text to dim
+ */
+echo " > " . terminal_style('This text will be dim', null, null, 'dim') . PHP_EOL;
+
+/*
+ * Change text to underlined
+ */
+echo " > " . terminal_style('This text will be underlined', null, null, 'underlined') . PHP_EOL;
+
+/*
+ * Change text to blink
+ */
+echo " > " . terminal_style('This text will be blink', null, null, 'blink') . PHP_EOL;
+
+/*
+ * See the GitHub page for all color names
+ * https://github.com/bvanhoekelen/terminal-style
+ */
